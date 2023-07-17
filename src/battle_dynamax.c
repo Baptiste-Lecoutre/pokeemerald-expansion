43cf6a8e1b28d2ed0b5ceef84b3a5673ee145184
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2023-07-17 20:38:43 +0200

    fix main merge build

--- src/battle_dynamax.c
+++ src/battle_dynamax.c
@@ -407,7 +407,7 @@ u8 GetMaxMovePower(u16 move)
 
 static u8 GetMaxPowerTier(u16 move)
 {
-    if (gBattleMoves[move].flags & FLAG_TWO_STRIKES)
+    if (gBattleMoves[move].twoStrikes)
     {
         switch(gBattleMoves[move].power)
         {
