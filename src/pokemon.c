631e809097ffb7eb419558a15dfc2817bafcb278
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2023-07-25 20:04:36 +0200

    game corner can now give mon with their HA

--- src/pokemon.c
+++ src/pokemon.c
@@ -3713,6 +3713,15 @@ void CreateBoxMon(struct BoxPokemon *boxMon, u16 species, u8 level, u8 fixedIV, 
     #endif
     }
 
+#if P_FLAG_FORCE_HIDDEN_ABILITY != 0
+    if (FlagGet(P_FLAG_FORCE_HIDDEN_ABILITY))
+    {
+        value = 2;
+        FlagClear(P_FLAG_FORCE_HIDDEN_ABILITY);
+        SetBoxMonData(boxMon, MON_DATA_ABILITY_NUM, &value);
+    }
+    else
+#endif
     if (gSpeciesInfo[species].abilities[1])
     {
         value = personality & 1;
