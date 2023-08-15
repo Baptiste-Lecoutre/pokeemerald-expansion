b684742c4e1d1f7c09226096ec68199b4d51271a
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2023-08-15 16:02:38 +0200

    fix build after merge

--- src/battle_util.c
+++ src/battle_util.c
@@ -10179,11 +10179,6 @@ bool32 CanMegaEvolve(u8 battlerId)
     // Check if there is an entry in the evolution table for Wish Mega Evolution.
     if (GetBattleFormChangeTargetSpecies(battlerId, FORM_CHANGE_BATTLE_MEGA_EVOLUTION_MOVE) != SPECIES_NONE)
     {
-        #if DEBUG_BATTLE_MENU == TRUE
-        if (gBattleStruct->debugHoldEffects[battlerId])
-            holdEffect = gBattleStruct->debugHoldEffects[battlerId];
-        else
-    #endif
         if (itemId == ITEM_ENIGMA_BERRY_E_READER)
             holdEffect = gEnigmaBerries[battlerId].holdEffect;
         else
