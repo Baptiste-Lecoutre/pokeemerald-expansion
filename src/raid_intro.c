427c0eea4a0a39e8c0c65549b40ed380e454f9c9
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2023-10-16 20:21:29 +0200

    add entry 0 for the gRaidPartners
    and shift temporary partners in raid intro screen

--- src/raid_intro.c
+++ src/raid_intro.c
@@ -739,11 +739,11 @@ static bool32 GetRaidBattleData(void)
 		for (i = 0; i < MAX_NUM_PARTNERS; i++)
 		{
 			struct Partner* partner = &sRaidBattleIntro->partners[i];
-			partner->id = gRaidPartners[i].id;
-			partner->graphicsId = gRaidPartners[i].graphicsId;
+			partner->id = gRaidPartners[i+1].id;
+			partner->graphicsId = gRaidPartners[i+1].graphicsId;
 
 			for (j = 0; j < MAX_TEAM_SIZE; j++)
-				partner->team[j] = gRaidPartners[i].team[j];
+				partner->team[j] = gRaidPartners[i+1].team[j];
 		}
 
 		return TRUE;
