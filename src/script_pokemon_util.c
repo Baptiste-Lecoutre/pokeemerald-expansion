a65c9616916e28f87ed801d7632622e6ff3fe244
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2023-12-12 22:38:42 +0100

    revert random egg veryday until I figure out what I want to do with it

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -104,11 +104,7 @@ u8 ScriptGiveEgg(u16 species)
     struct Pokemon mon;
     u8 isEgg;
 
-    if (gSaveBlock1Ptr->location.mapNum == MAP_NUM(LAVARIDGE_TOWN)
-        && gSaveBlock1Ptr->location.mapGroup == MAP_GROUP(LAVARIDGE_TOWN))
-        CreateEgg(&mon, species, TRUE);
-    else
-        CreateEgg(&mon, species, FALSE);
+    CreateEgg(&mon, species, TRUE);
     isEgg = TRUE;
     SetMonData(&mon, MON_DATA_IS_EGG, &isEgg);
 
