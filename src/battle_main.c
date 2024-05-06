9f3a53f6ccad11a99e5568c405e4e54d8e0bbecd
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2024-05-06 15:02:20 +0200

    remove unused variables in battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -2384,7 +2384,6 @@ u8 CreateNPCTrainerPartyFromTrainer(struct Pokemon *party, const struct Trainer 
 static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 firstTrainer)
 {
     u8 retVal;
-    struct Trainer trainer;
     if (trainerNum == TRAINER_SECRET_BASE)
         return 0;
     retVal = CreateNPCTrainerPartyFromTrainer(party, GetTrainerStructFromId(trainerNum), firstTrainer, gBattleTypeFlags);
@@ -2393,7 +2392,6 @@ static u8 CreateNPCTrainerParty(struct Pokemon *party, u16 trainerNum, bool8 fir
 
 void CreateTrainerPartyForPlayer(void)
 {
-    struct Trainer trainer;
     ZeroPlayerPartyMons();
     gPartnerTrainerId = gSpecialVar_0x8004;
     CreateNPCTrainerPartyFromTrainer(gPlayerParty, GetTrainerStructFromId(gSpecialVar_0x8004), TRUE, BATTLE_TYPE_TRAINER);
