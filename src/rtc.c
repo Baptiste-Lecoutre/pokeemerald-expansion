aea853a1ad689526fcb7b4ab1ff91c002f5dfc03
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2024-06-10 22:26:48 +0200

    fix build after merge and configs

--- src/rtc.c
+++ src/rtc.c
@@ -372,6 +372,7 @@ void FormatDecimalTimeWithoutSeconds(u8 *txtPtr, s8 hour, s8 minute, bool32 is24
     {
         txtPtr = ConvertIntToDecimalStringN(txtPtr, hour, STR_CONV_MODE_LEADING_ZEROS, 2);
         *txtPtr++ = CHAR_COLON;
+        *txtPtr++ = CHAR_SPACE;
         txtPtr = ConvertIntToDecimalStringN(txtPtr, minute, STR_CONV_MODE_LEADING_ZEROS, 2);
     }
     else
@@ -384,6 +385,7 @@ void FormatDecimalTimeWithoutSeconds(u8 *txtPtr, s8 hour, s8 minute, bool32 is24
             txtPtr = ConvertIntToDecimalStringN(txtPtr, hour - 12, STR_CONV_MODE_LEADING_ZEROS, 2);
 
         *txtPtr++ = CHAR_COLON;
+        *txtPtr++ = CHAR_SPACE;
         txtPtr = ConvertIntToDecimalStringN(txtPtr, minute, STR_CONV_MODE_LEADING_ZEROS, 2);
         txtPtr = StringAppend(txtPtr, gText_Space);
         if (hour < 12)
