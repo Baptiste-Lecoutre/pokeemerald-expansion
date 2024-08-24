b1fd313b4a0137ad2c2afcce4be64d2e09299d64
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2024-08-24 18:54:45 +0200

    port LandOfChocolate's Energized Emerald Mauville tileset
    and changed the bike shop building

--- src/tileset_anims.c
+++ src/tileset_anims.c
@@ -29,6 +29,7 @@ static void TilesetAnim_Rustboro(u16);
 static void TilesetAnim_Dewford(u16);
 static void TilesetAnim_Slateport(u16);
 static void TilesetAnim_Mauville(u16);
+static void TilesetAnim_Verdanturf(u16);
 static void TilesetAnim_Lavaridge(u16);
 static void TilesetAnim_EverGrande(u16);
 static void TilesetAnim_Pacifidlog(u16);
@@ -708,6 +709,13 @@ void InitTilesetAnim_Mauville(void)
     sSecondaryTilesetAnimCallback = TilesetAnim_Mauville;
 }
 
+void InitTilesetAnim_Verdanturf()
+{
+    sSecondaryTilesetAnimCounter = sPrimaryTilesetAnimCounter;
+    sSecondaryTilesetAnimCounterMax = sPrimaryTilesetAnimCounterMax;
+    sSecondaryTilesetAnimCallback = TilesetAnim_Verdanturf;
+}
+
 void InitTilesetAnim_Lavaridge(void)
 {
     sSecondaryTilesetAnimCounter = 0;
@@ -870,6 +878,26 @@ static void TilesetAnim_Slateport(u16 timer)
 }
 
 static void TilesetAnim_Mauville(u16 timer)
+{
+    if (timer % 8 == 0)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 0);
+    if (timer % 8 == 1)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 1);
+    if (timer % 8 == 2)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 2);
+    if (timer % 8 == 3)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 3);
+    if (timer % 8 == 4)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 4);
+    if (timer % 8 == 5)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 5);
+    if (timer % 8 == 6)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 6);
+    if (timer % 8 == 7)
+        QueueAnimTiles_Mauville_Flowers(timer / 8, 7);
+}
+
+static void TilesetAnim_Verdanturf(u16 timer)
 {
     if (timer % 8 == 0)
         QueueAnimTiles_Mauville_Flowers(timer / 8, 0);
