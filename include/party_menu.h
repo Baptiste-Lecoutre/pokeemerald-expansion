ec9da05dcc409cb4c4eb06650b735c38d54c6d8d
Baptiste-Lecoutre <59924283+Baptiste-Lecoutre@users.noreply.github.com>
2025-02-27 22:54:59 +0100

    party menu highlight from greenphx

--- include/party_menu.h
+++ include/party_menu.h
@@ -44,6 +44,9 @@ extern const struct SpriteTemplate gSpriteTemplate_StatusIcons;
 #define PARTY_MENU_STYLE            PARTY_MENU_STYLE_BW                       
 #define PARTY_MENU_ALPHA            (PARTY_MENU_STYLE == PARTY_MENU_STYLE_BW) // If TRUE, the columns have a transparent BG. 
                                                                               // Only designed for the BW style, so looks ugly on other styles.
+#define PARTY_MENU_HIGHLIGHT_ACTIVE_SINGLE TRUE // If TRUE, in single battles, changes the colour of the first party slot. Easier to see who is on the field, in my opinion.
+#define PARTY_MENU_HIGHLIGHT_ACTIVE_DOUBLE TRUE // If TRUE, in double battles, changes the colour of the first two party slots. Easier to see who is on the field, in my opinion.
+                                                 // This is not active in multi-battles, as it uses the colour that is usually used to represent our partner's pokemon.
 
 void AnimatePartySlot(u8 slot, u8 animNum);
 bool8 IsMultiBattle(void);
